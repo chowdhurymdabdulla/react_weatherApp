5b47355a0ca765a0e1d94caf0fdf161a0e93f935
Abdulla Chowdhury <chowdhurymdabdulla@gmail.com>
2016-12-22 16:14:08 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ var express = require('express');
 // create our app
 
 var app = express();
-
-var PORT = process.env.PORT || 9005;
+const PORT = process.env.PORT || 9005;
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+    } else {
+    res.redirect('http://' + req.hostname + req.url);
+    }
+});
 // we need to use Public Folder
 app.use(express.static('public'));
 
-app.listen(port, function(){
-  console.log(`Express Server is up on port: $(PORT)`);
+app.listen(PORT, function() {
+    console.log(`Express Server is up on PORT: ${PORT}`);
 });
